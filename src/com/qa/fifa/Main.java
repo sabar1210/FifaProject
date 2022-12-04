a6b7f7af5747a94c00571ac78fbcb16fdf057247
Sabar1210 <sabar@sabar.home>
2022-12-04 13:23:06 -0500

    2nd -commit

--- src/com/qa/fifa/Main.java
+++ src/com/qa/fifa/Main.java
@@ -3,7 +3,8 @@ package com.qa.fifa;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("I added a new calass in this project");
 
 	}
 
